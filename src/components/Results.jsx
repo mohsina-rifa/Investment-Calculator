f55ecc2975143194bfe8c8a4c1b6314e7f70e743
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-04-23 16:24:28 +0600

    Enhance Results component to display investment results in a table format and format investment values

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,30 @@
-import { calculateInvestmentResults } from "../util/investment.js";
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
 
 export default function Results({ input }) {
   const resultsData = calculateInvestmentResults(input);
-  console.log(resultsData);
 
-  return <p>Results</p>;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>({formatter.format(yearData.interest)})</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
