baab5e09975d8cf93edc8bb9434fd60dfc4b79d1
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-04-23 13:34:25 +0600

    Add Results component to display calculated investment results

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 
 function App() {
   const [userInput, setUserInput] = useState({
@@ -24,6 +25,7 @@ function App() {
     <>
       <Header />
       <UserInput input={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </>
   );
 }
